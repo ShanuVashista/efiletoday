8edee7becad80d4c70397ef22f001e43aeae8d31
manishprimelab <manish.saraswat@primelab.io>
2022-03-25 20:37:30 +0530

    modify profile

--- pages/api/users/login.ts
+++ pages/api/users/login.ts
@@ -1,52 +1,55 @@
 import dbConnect from "../../../lib/dbConnect";
 import User from "../../../models/User";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type {NextApiRequest, NextApiResponse} from "next";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-export default async function handler(
+export default async function handler (
     req: NextApiRequest,
     res: NextApiResponse
-) {
-    const { method } = req;
+){
+    const {method} = req;
     await dbConnect();
 
-    switch (method) {
+    switch (method){
         case "POST":
             try {
-                const user = await User.find({ "email": req.body.email });
-                if (user.length == 0) {
+                const user = await User.find({email: req.body.email});
+
+                if (user.length == 0){
                     res.status(404).json({
                         success: false,
                         data: "Credential Not Found"
                     });
-                } else {
-                    if (user[0].password != req.body.password) {
+                } else {                    
+                    if(!bcrypt.compareSync(req.body.password, user[0].password)){
                         res.status(400).json({
-                            success: false,
+                            success: false, 
                             data: "Wrong Credential"
                         });
                     } else {
-                        let token = jwt.sign({
-                            data: user
-                        }, 'efiletoday-key', { expiresIn: '1d' });
+                        const token = jwt.sign({_id: user[0]._id}, "efiletoday-key", {expiresIn: "1d"});
+                                                
                         res.status(200).json({
                             success: true,
                             accesstoken: token,
                             data: {
-                                "_id":user[0]._id,
-                                "name":user[0].name,
-                                "email":user[0].email
+                                _id: user[0]._id,
+                                firstName: user[0].firstName,
+                                lastName: user[0].lastName,
+                                email: user[0].email,
+                                phone: user[0].phone
                             }
                         });
                     }
                 }
-            } catch (error) {
-                res.status(400).json({ success: false });
+            } catch (error){
+                res.status(400).json({success: false});
             }
 
             break;
         default:
-            res.status(400).json({ success: false });
+            res.status(400).json({success: false});
             break;
     }
 }
