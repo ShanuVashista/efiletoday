1e652349df2ec47d98aaf22a8c7de0699ea75ef1
ashish primelab <ashish.singla@primelab.io>
2022-03-25 15:27:09 +0530

    mongo db setup

---
+++ lib/dbConnect.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+    function someFunction(): string;
+    // eslint-disable-next-line no-var
+    var mongoose: {
+        conn: mongoose.Connection | mongoose.Mongoose | null,
+        promise: Promise<mongoose.Connection | mongoose.Mongoose> | null,
+    };
+}
+let cached = global.mongoose;
+
+if (!cached){
+    cached = global.mongoose = {
+        conn: null,
+        promise: null
+    };
+}
+
+async function dbConnect (){
+    if (!MONGODB_URI){
+        throw new Error(
+            "Please define the MONGODB_URI environment variable inside .env.local"
+        );
+    }
+
+    if (cached.conn){
+        return cached.conn;
+    }
+
+    if (!cached.promise){
+        cached.promise = mongoose.connect(MONGODB_URI).then(mongoose => {
+            return mongoose;
+        });
+    }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+export default dbConnect;
--- models/User.ts
+++ models/User.ts
@@ -1,8 +1,9 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String
-})
-const model = mongoose.models.User || mongoose.model('User', UserSchema)
-export default model
+    name: String,
+    email: String
+});
+
+const model = mongoose.models.User || mongoose.model("User", UserSchema);
+export default model;
