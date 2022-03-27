e43ffa940ff9b5cfb1e51fb04486b91ee2861ad5
shiveshcshukla <shivesh.shukla@primelab.io>
2022-03-27 19:42:15 +0530

    added contactus and Business APi's

--- models/Business.model.ts
+++ models/Business.model.ts
@@ -1,70 +1,45 @@
 import mongoose, {Schema} from "mongoose";
 
 const BusinessSchema = new Schema({
-    businessInfo: {
-        businessName: {
-            type: String,
-            required: true,
-            maxlength: 100
-        },
-        nameControl: {type: String},
-        einNo: {
-            type: String,
-            required: true,
-        },
-        businessType: {
-            type: String,
-            required: true,
-        },
-        addline1: {
-            type: String,
-            required: true,
-            minlength: 8
-        },
-        addline2: {type: String},
-        country: {
-            type: String,
-            required: true
-        },
-        state: {
-            type: String,
-            required: true
-        },
-        city: {
-            type: String,
-            required: true,
-        },
-        zip: {
-            type: String,
-            required: true
-        },
-        phone: {type: String},
+   
+    businessName: {
+        type: String,
+        required: true,
+        maxlength: 100
     },
-    poinOfContact: {
-        name: {type: String},
-        email: {type: String},
-        phone: {type: String}
+    nameControl: {type: String},
+    einNo: {
+        type: String,
+        required: true,
     },
-    signingAuth: {
-        name: {
-            type: String,
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        phone: {
-            type: String,
-            required: true
-        }
-    }
-
-});
+    businessType: {
+        type: String,
+        required: true,
+    },
+    addline1: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    addline2: {type: String},
+    country: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    phone: {type: String},
+}, {timestamps: true});
 
 const model = mongoose.models.Business || mongoose.model("Business", BusinessSchema);
 export default model;
