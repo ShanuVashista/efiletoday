c639ccab9e388a007ccf476b54f96969ed9c3199
shiveshcshukla <shivesh.shukla@primelab.io>
2022-03-27 21:41:05 +0530

    commented specific shema in business

--- models/Business.model.ts
+++ models/Business.model.ts
@@ -1,64 +1,66 @@
 import mongoose, {Schema} from "mongoose";
 
-const BusinessAddressSchema = new Schema({
-    addline1: {
-        type: String,
-        required: true,
-        minlength: 8
-    },
-    addline2: {type: String},
-    country: {
-        type: String,
-        required: true
-    },
-    state: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true,
-    },
-    zip: {
-        type: String,
-        required: true
-    }
-});
+// We can also use specific Schema for these Fields if we need specific Id for these.
 
-const SigningAutheritySchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    }
+// const BusinessAddressSchema = new Schema({
+//     addline1: {
+//         type: String,
+//         required: true,
+//         minlength: 8
+//     },
+//     addline2: {type: String},
+//     country: {
+//         type: String,
+//         required: true
+//     },
+//     state: {
+//         type: String,
+//         required: true
+//     },
+//     city: {
+//         type: String,
+//         required: true,
+//     },
+//     zip: {
+//         type: String,
+//         required: true
+//     }
+// });
 
-});
+// const SigningAutheritySchema = new Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true
+//     },
+//     phone: {
+//         type: String,
+//         required: true
+//     }
 
-const PointOfContactSchema = new Schema ({
-    userName: {
-        type: String,
-        required: true
-    },
-    userEmail: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: String,
-        required: true
-    }
-});
+// });
+
+// const PointOfContactSchema = new Schema ({
+//     userName: {
+//         type: String,
+//         required: true
+//     },
+//     userEmail: {
+//         type: String,
+//         required: true,
+//     },
+//     userPhone: {
+//         type: String,
+//         required: true
+//     }
+// });
 
 const BusinessSchema = new Schema({
    
@@ -76,9 +78,62 @@ const BusinessSchema = new Schema({
         type: String,
         required: true,
     },
-    businessAdd: BusinessAddressSchema,
-    signingAuth: SigningAutheritySchema,
-    pointOfContact: PointOfContactSchema,
+    businessAdd: {
+        addline1: {
+            type: String,
+            required: true,
+            minlength: 8
+        },
+        addline2: {type: String},
+        country: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        zip: {
+            type: String,
+            required: true
+        }
+    },
+    signingAuth: {
+        name: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        }
+    },
+    pointOfContact: {
+        userName: {
+            type: String,
+            required: true
+        },
+        userEmail: {
+            type: String,
+            required: true,
+        },
+        userPhone: {
+            type: String,
+            required: true
+        }
+    },
     businessPhone: {type: String},
 }, {timestamps: true});
 
