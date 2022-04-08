4aa6225cbcb121750e903287eab8a4ff2819b611
Developer <asim.ansari@primelab.io>
2022-04-08 20:16:30 +0530

    Header issue fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import {Button, Drawer, Grid, Hidden, IconButton, Typography} from "@mui/material";
+import {Button, Drawer, Grid, IconButton, Typography} from "@mui/material";
 import Image from "next/image";
 import logo from "../public/logo.png";
 import {BsFillTelephoneFill} from "react-icons/bs";
@@ -9,7 +9,7 @@ import {AiOutlineInstagram, AiOutlineTwitter} from "react-icons/ai";
 import {FaFacebookF} from "react-icons/fa";
 import {RiLinkedinFill} from "react-icons/ri";
 
-const Header = (): JSX.Element => {
+const Header = () => {
     const [show, setShow] = useState(false);
 
     const links = [
@@ -59,7 +59,7 @@ const Header = (): JSX.Element => {
     ];
 
     const content = <>{
-        links.map((link, i) => <Grid key={i} className="p-2 p-md-2">
+        links.map((link, i) => <Grid key={i} className="p-1 p-lg-2 w-auto">
             <Button size="small" className="header-link">
                 {link.label}
             </Button>
@@ -95,22 +95,20 @@ const Header = (): JSX.Element => {
             </Grid>
             <Grid container direction="column" wrap="nowrap" item xs>
                 <Grid container wrap="nowrap" alignItems="center" justifyContent="space-between" className="p-2 text-white">
-                    <Hidden mdDown>
-                        <Grid item xs container>
-                            <Grid container wrap="nowrap" className="w-auto p-2" alignItems="center">
-                                <BsFillTelephoneFill/>
-                                <Typography component={"a"} href={"tel:'+14146906435'"} style={{marginLeft: ".5rem"}}>
-                                    +1 (414) 690-6435 |
-                                </Typography>
-                            </Grid>
-                            <Grid container wrap="nowrap" className="w-auto p-2" alignItems="center">
-                                <HiOutlineMailOpen fontSize="large"/>
-                                <Typography component={"a"} href={"mailto:'info@efiletoday.com'"} style={{marginLeft: ".5rem"}}>
-                                    info@efiletoday.com
-                                </Typography>
-                            </Grid>
+                    <Grid item xs container className="hidden-mdUp-container">
+                        <Grid container wrap="nowrap" className="w-auto p-2" alignItems="center">
+                            <BsFillTelephoneFill/>
+                            <Typography component={"a"} href={"tel:'+14146906435'"} style={{marginLeft: ".5rem"}}>
+                                +1 (414) 690-6435 |
+                            </Typography>
                         </Grid>
-                    </Hidden>
+                        <Grid container wrap="nowrap" className="w-auto p-2" alignItems="center">
+                            <HiOutlineMailOpen fontSize="large"/>
+                            <Typography component={"a"} href={"mailto:'info@efiletoday.com'"} style={{marginLeft: ".5rem"}}>
+                                info@efiletoday.com
+                            </Typography>
+                        </Grid>
+                    </Grid>
                     {
                         socialIcon.map((icon, i) => <Grid key={i} className="p-1">
                             <Grid
@@ -131,38 +129,34 @@ const Header = (): JSX.Element => {
                         </Grid>)
                         
                     }
-                    <Hidden mdUp>
-                        <Grid className="p-1">
-                            <IconButton
-                                size="small"
-                                style={
-                                    {
-                                        width: 30,
-                                        height: 30,
-                                        borderWidth: 1,
-                                        borderRadius: "50%",
-                                        borderStyle: "solid",
-                                        borderColor: "white",
-                                    }
+                    <Grid className="p-1 hidden-mdDown-container">
+                        <IconButton
+                            size="small"
+                            style={
+                                {
+                                    width: 30,
+                                    height: 30,
+                                    borderWidth: 1,
+                                    borderRadius: "50%",
+                                    borderStyle: "solid",
+                                    borderColor: "white",
                                 }
-                            >
-                                <Search color="white" size={15}/>
-                            </IconButton>
-                        </Grid>
-                    </Hidden>
+                            }
+                        >
+                            <Search color="white" size={15}/>
+                        </IconButton>
+                    </Grid>
                 </Grid>
-                <Grid container alignItems="center" wrap="nowrap" justifyContent={"space-between"}>
-                    <Hidden mdDown>
+                <Grid container alignItems="center" wrap="nowrap" justifyContent="space-between">
+                    <Grid item xs container alignItems="center" justifyContent="space-between" wrap="nowrap" className="hidden-mdUp-container">
                         {content}
-                    </Hidden>
-                    <Hidden mdDown>
-                        <Grid className="p-2">
-                            <IconButton>
-                                <Search color="white" size={20}/>
-                            </IconButton>
-                        </Grid>
-                    </Hidden>
-                    <Grid item md={2} className="p-3 p-lg-0">
+                    </Grid>
+                    <Grid className="p-2 hidden-mdUp-container">
+                        <IconButton>
+                            <Search color="white" size={20}/>
+                        </IconButton>
+                    </Grid>
+                    <Grid container item md={2} className="p-2 p-lg-3" justifyContent="flex-end">
                         <Button
                             variant="contained"
                             size="small"
@@ -178,25 +172,23 @@ const Header = (): JSX.Element => {
                             Login/Register
                         </Button>
                     </Grid>
-                    <Hidden mdUp>
-                        <Grid item md={2} className="p-2 p-lg-0">
-                            <IconButton
-                                onClick={() => setShow(true)}
-                                size="small"
-                                style={
-                                    {
-                                        borderWidth: 1,
-                                        marginRight: 2,
-                                        borderRadius: "50%",
-                                        borderStyle: "solid",
-                                        borderColor: "white",
-                                    }
+                    <Grid item md={2} className="p-2 p-lg-0 hidden-mdDown-container">
+                        <IconButton
+                            onClick={() => setShow(true)}
+                            size="small"
+                            style={
+                                {
+                                    borderWidth: 1,
+                                    marginRight: 2,
+                                    borderRadius: "50%",
+                                    borderStyle: "solid",
+                                    borderColor: "white",
                                 }
-                            >
-                                <Menu color="white" size={22}/>
-                            </IconButton>
-                        </Grid>
-                    </Hidden>
+                            }
+                        >
+                            <Menu color="white" size={22}/>
+                        </IconButton>
+                    </Grid>
                 </Grid>
             </Grid>
             <Drawer
