9c8eb483bca466bffdffbe3d0172e02c496cbe37
Developer <asim.ansari@primelab.io>
2022-04-24 20:36:18 +0530

    Resolved again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import "../styles/globals.scss";
+import "../styles/globals.css";
 import type {AppProps} from "next/app";
-import React from "react";
 
 function MyApp ({Component, pageProps}: AppProps){
+    // eslint-disable-next-line react/react-in-jsx-scope
     return <Component {...pageProps} />;
 }
 
-export default MyApp; 
+export default MyApp;
